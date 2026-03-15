73961ccebffef81a70f31d746c2caf7f59e855c7
Ysabella Santos <ysasantosbella2005@gmail.com>
2026-03-15 15:00:14 +0800

    oop show example toggle

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggleExplanation(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
